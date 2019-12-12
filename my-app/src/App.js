5947334f848837d1680946c9f70c954d8c317d77
Daniel <danielgorgis1996@gmail.com>
2019-12-12 01:38:37 +0100

    Start commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import loogo from './loogo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={loogo} className="App-logo" alt="loogo" />
+        <p>
+          Tag Testen!
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
